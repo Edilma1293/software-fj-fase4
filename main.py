7d78db1bb446af31ae275a792f0ace63eb49ab20
Edilma1293 <esepulvedag@unadvirtual.edu.co>
2026-05-05 14:04:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,23 @@ def registrar_log(error):
     with open("logs.txt", "a") as archivo:
         archivo.write(str(error) + "\n")
 
+print("=== SISTEMA SOFTWARE FJ ===")
+
+# OPERACIÓN 1: RESERVA VALIDA
 try:
-    cliente1 = Cliente("Edy", "12345")
+    nombre = input("Ingrese nombre del cliente: ")
+    documento = input("Ingrese documento del cliente: ")
+
+    cliente1 = Cliente(nombre, documento)
+
     servicio1 = ReservaSala("Sala VIP", 50)
 
-    reserva1 = Reserva(cliente1, servicio1, 3)
+    dias = int(input("Ingrese días de reserva: "))
+
+    reserva1 = Reserva(cliente1, servicio1, dias)
     reserva1.confirmar()
 
+    print("\n--- RESERVA EXITOSA ---")
     print("Cliente:", cliente1)
     print("Estado:", reserva1.estado)
     print("Costo total:", reserva1.procesar())
@@ -21,9 +31,9 @@ except Exception as e:
     registrar_log(e)
     print("Error:", e)
 
+# OPERACIÓN 2: ERROR CONTROLADO (CLIENTE INVÁLIDO)
 try:
     cliente2 = Cliente("", "12")
 except Exception as e:
     registrar_log(e)
     print("Error controlado:", e)
-  
