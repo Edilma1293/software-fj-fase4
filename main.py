c45abe24841a95b84d0f6c4c83e743695132d9a3
Edilma1293 <esepulvedag@unadvirtual.edu.co>
2026-05-05 13:00:34 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+from cliente import Cliente
+from servicio import ReservaSala
+from reserva import Reserva
+
+def registrar_log(error):
+    with open("logs.txt", "a") as archivo:
+        archivo.write(str(error) + "\n")
+
+try:
+    cliente1 = Cliente("Ana", "12345")
+    servicio1 = ReservaSala("Sala VIP", 50)
+
+    reserva1 = Reserva(cliente1, servicio1, 3)
+    reserva1.confirmar()
+
+    print("Cliente:", cliente1)
+    print("Estado:", reserva1.estado)
+    print("Costo total:", reserva1.procesar())
+
+except Exception as e:
+    registrar_log(e)
+    print("Error:", e)
+
+try:
+    cliente2 = Cliente("", "12")
+except Exception as e:
+    registrar_log(e)
+    print("Error controlado:", e)
+  
